6dbfabc707369a6730c387f3e7943146c87dfe8e
Renee Ishikawa <renishi58@gmail.com>
2022-09-20 00:01:19 -1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {advice: ''};
+
+    componentDidMount() {   // this executes right when the component renders
+        this.fetchAdvice();     // calling the function by using 'this.'
+        console.log('Component did mount');
+    }
+    
+    fetchAdvice = () => {   // this doesn't need a const because this is a method which means it's a function inside a class which makes it a method.
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response)=>{
+            //    console.log(response);
+            //    console.log(response.data);
+            //    console.log(response.data.slip);
+            //    console.log(response.data.slip.advice);     // this is the level that we need to get the advice and going to use this to destructure.
+                // example of destructuring
+                const { advice } = response.data.slip; // getting the data from the api and destructuring it to the object advice
+                this.setState({ advice })
+            })
+            .catch ((error)=>{
+                console.log(error);
+            });
+
+    }
+
+    render() {
+        const {advice} = this.state;
+        return (
+            <div className='app'>  
+                <div className='card'>
+                    <h1 className='heading'>{advice}</h1>
+                    <button className='button' onClick={this.fetchAdvice}>
+                        Give Me Advice!
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+//import ReactDOM from 'react-dom'; // received a warning here saying this is deprecated
+    // use this line instead
+import ReactDOM from 'react-dom/client'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// ReactDOM.render(<App />,document.getElementById('root')); // received a warning here saying this is deprecated
+    // use this instead
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
